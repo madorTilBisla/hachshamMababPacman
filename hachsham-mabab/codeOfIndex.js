3edb98e9579d665709355cbb41a02ddcd87df244
madorTilBisla <64409705+madorTilBisla@users.noreply.github.com>
2020-05-13 14:34:20 +0300

    Add files via upload

---
+++ hachsham-mabab/codeOfIndex.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function(){
+    document.addEventListener("click", nextScreen);
+});
+
+// We listen to the resize event
+window.addEventListener('resize', function(){
+    // We execute the same script as before
+    var vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+function nextScreen(){
+    window.location.href = "main.html";
+}
